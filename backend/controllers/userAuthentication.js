371432fda48a333f860ae2ebd8206d41db3614cf
pankaj0695 <pankajgupta0695@gmail.com>
2025-08-11 12:17:09 +0530

    update

--- backend/controllers/userAuthentication.js
+++ backend/controllers/userAuthentication.js
@@ -1,55 +1,52 @@
-const User = require("../models/user")
-const validate = require('../utils/validate');
+const User = require("../models/user");
+const validate = require("../utils/validate");
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
+const register = async (req, res) => {
+  try {
+    validate(req.body);
+    const { firstName, emailId, password, image, phoneno, city, country, bio } =
+      req.body;
+    req.body.password = await bcrypt.hash(password, 10);
+    req.body.role = "user";
+    // Create the user
+    const user = await User.create(req.body);
+    const token = jwt.sign(
+      { _id: user._id, emailId: emailId },
+      process.env.JWT_KEY,
+      { expiresIn: 60 * 60 }
+    );
+    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
+    res.status(201).send("User Registered Successfully");
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+};
 
+const login = async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
-const register = async(req,res)=>{
-    try{
-        validate(req.body);
-        const {firstName,emailId,password,image,phoneno,city,country,bio} = req.body;
-        req.body.password = await bcrypt.hash(password, 10);
-        req.body.role = 'user';
-        // Create the user
-        const user =  await User.create(req.body);
-        const token =  jwt.sign({_id:user._id , emailId:emailId},process.env.JWT_KEY,{expiresIn: 60*60});
-        res.cookie('token',token,{maxAge: 60*60*1000});
-        res.status(201).send("User Registered Successfully");
-    }
-    catch(err){
-        res.status(400).send("Error: "+err.message);
-    }
-}
+    if (!emailId) throw new Error("Invalid Credentials");
+    if (!password) throw new Error("Invalid Credentials");
 
-const login = async (req,res)=>{
+    const user = await User.findOne({ emailId });
 
-    try{
-        const {emailId, password} = req.body;
+    const match = await bcrypt.compare(password, user.password);
 
-        if(!emailId)
-            throw new Error("Invalid Credentials");
-        if(!password)
-            throw new Error("Invalid Credentials");
+    if (!match) throw new Error("Invalid Credentials");
 
-        const user = await User.findOne({emailId});
+    const token = jwt.sign(
+      { _id: user._id, emailId: emailId },
+      process.env.JWT_KEY,
+      { expiresIn: 60 * 60 }
+    );
+    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
+    res.status(200).send("Logged In Succeessfully");
+  } catch (err) {
+    res.status(401).send("Error: " + err.message);
+  }
+};
 
-        const match = await bcrypt.compare(password,user.password);
-
-        if(!match)
-            throw new Error("Invalid Credentials");
-
-        const token =  jwt.sign({_id:user._id , emailId:emailId},process.env.JWT_KEY,{expiresIn: 60*60});
-        res.cookie('token',token,{maxAge: 60*60*1000});
-        res.status(200).send("Logged In Succeessfully");
-    }
-    catch(err){
-        res.status(401).send("Error: "+err.message);
-    }
-}
-
-
-
-
-
-module.exports = {register,login};
+module.exports = { register, login };
--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const authRouter = express.Router();
-const {register,login} = require('../controllers/Userauthentication');
+const { register, login } = require("../controllers/userAuthentication");
 const userMiddleware = require("../middleware/userMiddleware");
-const adminMiddleware = require('../middleware/adminMiddleware');
-// Register 
+const adminMiddleware = require("../middleware/adminMiddleware");
+// Register
 // Log in
 // Log out
 // Get Profile
-authRouter.post('/register',register);
-authRouter.post('/login',login);
+authRouter.post("/register", register);
+authRouter.post("/login", login);
 // authRouter.post('/logout', userMiddleware, logout);
 // authRouter.post('/admin/register', adminMiddleware,adminRegister);
 // authRouter.delete('/deleteProfile',userMiddleware,deleteProfile);
